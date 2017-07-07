7dfec3fdf38907e4434bbd6615597f33cddc057d
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2017-07-07 14:18:30 +0200

    q4 finnished (see figure_q4.png)
    now adding better documentation

--- homework/week_1/q4.py
+++ homework/week_1/q4.py
@@ -2,84 +2,60 @@
 
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 
 # Read in Z,A,N from 'toiee.dat'
-isotopes = np.genfromtxt('toiee.dat',skip_header=1, usecols=(1,2,5))
+isotopes = np.genfromtxt('toiee.dat',skip_header=1, usecols=(1,2))
 """
 Columns are:
     0: A
     1: Z
-    2: N
 """
-#print 'isotopes', isotopes[0:3]
 
-
-# Read in spin from 'toiee.dat' into separate array
+# Read in spin and energy from 'toiee.dat' into separate 1-d array
 j_pi_info = np.genfromtxt('toiee.dat',skip_header=1, usecols=(7), dtype='string')
 energy = np.genfromtxt('toiee.dat',skip_header=1, usecols=(6))
 
-step_out = 0
-for i in range(len(j_pi_info)):
+# Empty lists to store the energy of the 
+e_state_six = []
+e_state_eight = []
+mass_nr = []
 
+for i in range(len(j_pi_info)):
     if j_pi_info[i] == '6+':
 
         nucleus = isotopes[i]
         A = isotopes[i,0]
-        
         Z = isotopes[i,1]
 
-        #for n in range(i,len(j_pi_info)):
         n = i
+        step_out = 0
         while step_out == 0:
 
             if j_pi_info[n] == '8+':
-                print nucleus
-                sys.exit()
+                e_state_six.append(energy[i])
+                e_state_eight.append(energy[n])
+                mass_nr.append(isotopes[n,0])
                 step_out = 1
 
             if isotopes[n,0] != A:
-                print 'hola'
+                #print 'hola'
                 step_out = 1
             if isotopes[n,1] != Z:
-                print 'hola2'
+                #print 'hola2'
                 step_out = 1
 
             n += 1
 
-           
+e_ratio = np.array(e_state_eight)/np.array(e_state_six)
 
+plt.plot(mass_nr, e_ratio, 'x')
+plt.plot(mass_nr, (12./7.)*np.ones(len(mass_nr)))
+plt.plot(mass_nr, np.ones(len(mass_nr)))
+plt.xlabel('Mass nr A')
+plt.ylabel('Spin ratio 8+/6+')
+plt.legend(['Ratio', '1', '12/7'])
+plt.title('Question 4, HW1')
 
+plt.show()
 
-
-
-
-#print j_pi_info
-
-"""
-
-for i in range(0,len(isotopes)):
-    # Only check if isotope has > 2 protons
-    if isotopes[i,0] <= 2:
-        continue
-
-    z = isotopes[i,0]
-    a = isotopes[i,1]
-    n = isotopes[i,2]
-
-    # Check if unbound to 2p-decay
-    p2_isotope = np.where((isotopes[:,0] == z-2) & (isotopes[:,2] == n))
-
-    # Skip if there isn't a measured isotope with 2 fewer protons
-    if len(p2_isotope[0]) == 0:
-        continue
-
-    # If B.E. of isotope with 2 fewer protons is smaller, skip
-    if bind_energies[p2_isotope] - bind_energies[i] < 0:
-        continue
-
-    ## Check if bound to 1p-decay
-    p1_isotope = np.where((isotopes[:,0] == z-1) & (isotopes[:,2] == n))
-    # If B.E. of isotope with 1 fewer protons is smaller, print
-    if bind_energies[p1_isotope] - bind_energies[i] < 0:
-        print z,n,a
-"""
