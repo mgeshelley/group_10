ee44f3a6c44b3b965d143b7db6c7cdbe22fa82bb
GianlucaSalvioni <gianlucasalvioni@gmail.com>
2017-07-07 18:25:42 +0200

    corrected and commented version

--- homework/week_1/dripline_liquid_drop.py
+++ homework/week_1/dripline_liquid_drop.py
@@ -1,39 +1,36 @@
-# commented
+# program to solve A. Brown's homework ex.2 week1
+# this program calculate the neutron dripline using the 
+# liquid-drop model (LDM)
 import math
-# from math import sqrt, exp, log, sin
-# from math import *
-# import math as m
-# m is now the name of the math module
-#from math import sin as s, cos as c, log as ln
-#import numpy as np
-
+# parameters given in Brown's notes
 a1 = 15.49
 a2 = 17.23
 a3 = 0.697
-a4 = 22.6
-
+a4 = 22.6 
+# input data
 Zmin = 36
 Zmax = 44
-
-Z = Zmin
+#
+print '--------------------------------'
 print 'driplines from liquid-drop model'
-print '    Z     N     A     BE/A '
-while Z <= Zmax:
+print '  Z   N    A     BE/A '
+# loop over Z
+for Z in range(Zmin, Zmax+1,1):
     N = Z
     drip = 0
     while drip == 0:
         A = N+Z
-        BE = a1*A-a2*A**(1./3)-a3*Z**2*A**(-1./3)-a4*(N-Z)**2/A # binding energy defined > 0 for bound nuclei
+        BE = a1*A-a2*A**(2./3)-a3*Z**2/A**(1./3)-a4/A*(N-Z)**2  # binding energy defined > 0 for bound nuclei
         if BE > 0: 
              N = N+1
              drip = 0
         else: # BE < 0 i.e. unbound nuclei
-             C = A-1 # identify the previous A
-             N = N-1 # identify the previous N
-             BE = a1*A-a2*A**(1./3)-a3*Z**2*A**(-1./3)-a4*(N-Z)**2/A 
-             print '%5d %5d %5d %.3f' % (Z, A-Z, A, BE/A)
-             Z = Z+1
+             A = A-1 # identify the last bound A
+             N = N-1 # identify the last bound N
+             BE = a1*A-a2*A**(2./3)-a3*Z**2/A**(1./3)-a4/A*(N-Z)**2 # calculate the binding energy of the last bound 
+             print '%3d %4d %4d %7.3f' % (Z, A-Z, A, BE/A)
              drip = 1
     #end while 
-#end while
-
+#end for
+print '--------------------------------'
+#the results is benchmarked with https://upload.wikimedia.org/wikipedia/commons/3/3b/Bethe-Weizs%C3%A4cker.png for Z=25
