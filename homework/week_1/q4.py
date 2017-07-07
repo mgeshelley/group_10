84756688774164c59eefa6673761a85752883d01
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2017-07-07 15:05:09 +0200

    hw1: plotted against proton nr

--- homework/week_1/q4.py
+++ homework/week_1/q4.py
@@ -10,6 +10,7 @@ isotopes = np.genfromtxt('toiee.dat',skip_header=1, usecols=(1,2))
 Columns are:
     0: A
     1: Z
+
 """
 
 # Read in spin and energy from 'toiee.dat' into separate 1-d array
@@ -20,6 +21,7 @@ energy = np.genfromtxt('toiee.dat',skip_header=1, usecols=(6))
 e_state_six = []
 e_state_eight = []
 mass_nr = []
+z_number = []
 
 # Here we search for the states that includes both 6+ and 8+ states and stores the values of those states
 for i in range(len(j_pi_info)):
@@ -37,6 +39,7 @@ for i in range(len(j_pi_info)):
                 e_state_six.append(energy[i])
                 e_state_eight.append(energy[n])
                 mass_nr.append(isotopes[n,0])
+                z_number.append(isotopes[n,1])
                 step_out = 1
 
             if isotopes[n,0] != A:
@@ -49,15 +52,19 @@ for i in range(len(j_pi_info)):
 # The ratio of the energy of the states 8+/6+
 e_ratio = np.array(e_state_eight)/np.array(e_state_six)
 
-# Plotting the ratio against the mass nr + the lines at y=[12/2, 1]
-plt.plot(mass_nr, e_ratio, 'x')
-plt.plot(mass_nr, (12./7.)*np.ones(len(mass_nr)))
-plt.plot(mass_nr, np.ones(len(mass_nr)))
+N = mass_nr - np.array(z_number)
+
+# Plotting the ratio against the proton nr + the lines at y=[12/2, 1]
+
+#plt.plot(mass_nr, e_ratio, 'x')
+plt.plot(z_number, e_ratio, 'x')
+plt.plot(z_number, (12./7.)*np.ones(len(z_number)))
+plt.plot(z_number, np.ones(len(z_number)))
 
 # These statements makes a pretty plot:
-plt.xlabel('Mass nr A')
+plt.xlabel('Neutron nr')
 plt.ylabel('Spin ratio 8+/6+')
-plt.legend(['Ratio', '1', '12/7'])
+plt.legend(['Ratio', '12/7', '1'])
 plt.title('Question 4, HW1')
 
 # This statement shows the plot, but you can also save the figure directly with savefig('filename'):
