8c9460a250e5482dd65a782e8e9982f8668c9253
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2017-07-07 14:27:17 +0200

    q4 (hw): added some comments

--- homework/week_1/q4.py
+++ homework/week_1/q4.py
@@ -16,11 +16,12 @@ Columns are:
 j_pi_info = np.genfromtxt('toiee.dat',skip_header=1, usecols=(7), dtype='string')
 energy = np.genfromtxt('toiee.dat',skip_header=1, usecols=(6))
 
-# Empty lists to store the energy of the 
+# Empty lists to store the energy and mass number of the states that has both a 6+ and a 8+ state
 e_state_six = []
 e_state_eight = []
 mass_nr = []
 
+# Here we search for the states that includes both 6+ and 8+ states and stores the values of those states
 for i in range(len(j_pi_info)):
     if j_pi_info[i] == '6+':
 
@@ -39,23 +40,26 @@ for i in range(len(j_pi_info)):
                 step_out = 1
 
             if isotopes[n,0] != A:
-                #print 'hola'
                 step_out = 1
             if isotopes[n,1] != Z:
-                #print 'hola2'
                 step_out = 1
 
             n += 1
 
+# The ratio of the energy of the states 8+/6+
 e_ratio = np.array(e_state_eight)/np.array(e_state_six)
 
+# Plotting the ratio against the mass nr + the lines at y=[12/2, 1]
 plt.plot(mass_nr, e_ratio, 'x')
 plt.plot(mass_nr, (12./7.)*np.ones(len(mass_nr)))
 plt.plot(mass_nr, np.ones(len(mass_nr)))
+
+# These statements makes a pretty plot:
 plt.xlabel('Mass nr A')
 plt.ylabel('Spin ratio 8+/6+')
 plt.legend(['Ratio', '1', '12/7'])
 plt.title('Question 4, HW1')
 
+# This statement shows the plot, but you can also save the figure directly with savefig('filename'):
 plt.show()
 
