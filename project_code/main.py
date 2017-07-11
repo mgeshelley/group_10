553cbca4fdf08b6bc941530ffb89090d580b3f4b
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2017-07-11 16:43:04 +0200

    main: done creating the pairing interaction

--- project_code/main.py
+++ project_code/main.py
@@ -11,6 +11,8 @@ jmin = 1
 jmax = 1
 isos = 1
 
+g = 1
+
 """
 # for the harmonic oscillator:
 nmax = 2
@@ -24,8 +26,8 @@ jmax = 5
 sp_basis_filename = '3s.sp'
 SD_filename = "3s_slater_det.sd"
 
-g = 1
-tbme_filename = "pairing_1.int" #this need to be updated with the current value of g
+
+tbme_filename = "pairing_g%s.int" %(g) #this need to be updated with the current value of g
 
 # read the number of particles in the system
 N_particles = 4
@@ -182,29 +184,30 @@ def create_tbme_pairing(tbme_filename,nr_sp_states,g):
 
 	Output 
 	
-	tbme_filename:	 	file that contains the tbme for the pairing interaction
-				1st line: tbme_dim, E0, E1, E2, E3, d, n, espon
-					tbme_dim: number of non-zero matrix elements
-					E0,E1,E2,E3 are the energy of the sp levels (p-1 in our case)
-                                        d,n,espon 
-                                other lines: sp_1, sp_2, sp_3, sp_4, J_tot, T_tot, matrix element				
+	tbme_filename:	 	file that contains the tbme (two body matrix element) for the pairing interaction
+						1st line: tbme_dim, E0, E1, E2, E3, constant, A_core, exponent
+						tbme_dim: number of non-zero matrix elements
+						E0,E1,E2,E3 are the energy of the sp levels (p-1 in our case)
+                		A_core,constant,exponent: the parameters of the mass dependence [constant/(A_core+n_val)]^exponent
+                		other lines: sp_1, sp_2, sp_3, sp_4, J_tot, T_tot, matrix element
+
 	"""
 
 
 	# g is the value of the pairing parameter
 	index = 0
 	tbme_list = []
-        J = 0
-        T = 1
+	J = 0
+	T = 1
 
 	p_1=1
 	p_2=2
 	p_3=3
 	p_4=4
 
-        d= 0
-        n= 0
-        espon= 0
+	constant= 0
+	A_core= 0
+	exponent= 0
 
 	nr_sp_states = int(nr_sp_states)
 
@@ -214,30 +217,50 @@ def create_tbme_pairing(tbme_filename,nr_sp_states,g):
 			tbme_list.extend([a,a+1,b,b+1,J,T,-g])
 	dim_tbme = index
 	tbme_array = np.array(tbme_list)
-	tmbe_matrix = tbme_array.reshape(dim_tbme,7)
+	tbme_matrix = tbme_array.reshape(dim_tbme,7)
 
 
 	out_tbme = open(tbme_filename,"w")
 	out_tbme.write("! %s matrix elements\n" % (tbme_filename))
 	out_tbme.write("! list of states\n")
-	out_tbme.write("%d 7%7.3f" % (dim_tbme, p_0-1, p_1-1, p_2-1, p_3-1, d, n, espon))
+	out_tbme.write("%d %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f \n" % (dim_tbme, p_1-1, p_2-1, p_3-1, p_4-1, constant, A_core, exponent))
 	for i in range(0,dim_tbme):
-		out_tbme.write('6%2d %7.3f \n' % (tbme_matrix[i,0:5],tbme_matrix[i,6]))
+		out_tbme.write('%2d %2d %2d %2d %2d %2d %7.3f \n' % (tuple(tbme_matrix[i,0:6])+tuple([tbme_matrix[i,6]])))
 	out_tbme.close()
 	
 ##############################################################
 
-if os.path.isfile(sp_basis_filename) == False:
+#checking if the basis file exist, if not create the basis
+if os.path.isfile(sp_basis_filename) == False: 
 	sp_pairing(nmin, nmax, lmin, lmax, jmin, jmax, isos)
 
 particle, A_core, Z_core, nr_sp_states, nr_groups, nr_sp_n, nr_sp_p, sp_matrix = read_basis(sp_basis_filename)
 
+#creating the slater determinants
 if os.path.isfile(SD_filename) == False:
 	create_SD(N_particles, nr_sp_states, sp_matrix, SD_filename)
 
 SD_matrix = read_SD(SD_filename)
 
+#creating the pairing interaction
+if os.path.isfile(tbme_filename) == False:
+	create_tbme_pairing(tbme_filename,nr_sp_states,g)
 
-print SD_matrix
+
+
+
+# Finding the eigenvalues and eigenvectors
+##############################################################
+eigval, eigvec = np.linalg.eigh(hamiltonian_matrix)
+
+
+
+
+
+
+
+
+
+
 
 
