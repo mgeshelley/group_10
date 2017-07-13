ab8c7c9b95deb6ab79a955fa088d3eea822f2eb6
Gianluca Salvioni <gianlucasalvioni@gmail.com>
2017-07-13 17:19:14 +0200

    ham.py: 2-body pairing hamiltonian corrected

--- project_code/ham.py
+++ project_code/ham.py
@@ -87,59 +87,109 @@ def Hamiltonian_one_body(N_particles, nr_sp_states, SD_filename, tbme_filename):
 						hamiltonian[beta, alpha] = hamiltonian[beta, alpha] + H_diag[p-1,q-1]
 	# print the Hamiltonian matrix
 	# print hamiltonian
-	return SD_matrix
+	return hamiltonian
 
 ##########################################################################################################
 
 
+#def Hamiltonian_one_body(N_particles, nr_sp_states, SD_filename, tbme_filename):
+def Hamiltonian():  #used only for testing 
 
-def Hamiltonian():
-	counter = 0 
-	# s_d is slater determinant
-	s_d = np.loadtxt(folder_name+"3s_slater_det.sd", comments = "!", skiprows=0)
-
-	s_d2 = np.loadtxt(folder_name+"3s_slater_det.sd", comments = "!", skiprows=0)
+	# The file with Slater Determinants (s_d) is loaded [index, sp states]
+	s_d = np.loadtxt(folder_name+"3s_slater_det.sd", comments = "!", skiprows=0) #only for testing
+	#s_d = np.loadtxt(SD_filename, comments = "!", skiprows=0)
 	
-	# nos is number of states
-	nos = s_d.shape[0]
+	# number of Slater determinants
+	nr_sd = s_d.shape[0]
 
-	# Read in two-body matrix elements
-	two_body_me = np.loadtxt(folder_name+"pairing_g1.int", comments = "!", skiprows=3)
-
+	# Read the two-body matrix elements from the .int file 
+	two_body_me = np.loadtxt(folder_name+"pairing_g1.int", comments = "!", skiprows=3) #only for testing
+	#two_body_me = np.loadtxt(tbme_filename, comments = "!", skiprows=3)
 	
 	
-	# nme is the number of two body matrix elements	
-	nme = two_body_me.shape[0]
-	if np.genfromtxt(folder_name+"pairing_g1.int", comments = "!", skip_header=2, max_rows=1)[0] != nme:
-		sys.exit("ERROR: Dimention of tbmatrix not consistent!!!")
+	# nr_2bme is the number of two body matrix elements	
+	nr_2bme = two_body_me.shape[0]
+	if np.genfromtxt(folder_name+"pairing_g1.int", comments = "!", skip_header=2, max_rows=1)[0] != nr_2bme:
+		sys.exit("ERROR: Dimension of 2-body matrix not consistent!!!") #only for testing
+	#if np.genfromtxt(tbme_filename, comments = "!", skip_header=2, max_rows=1)[0] != nr_2bme:
+	#	sys.exit("ERROR: Dimension of 2-body matrix not consistent!!!")
 
 
-	# neo is the hamiltonian with zero elements
-	neo = np.zeros((nos, nos))
+	# initialize to zero the Hamiltonian <beta_SD|H|alpha_SD>
+	hamiltonian_2body = np.zeros((nr_sd, nr_sd))
 
-	# Starting loop over NSD
-	for beta in range(0, nos, 1):
-		for alpha in range(0, nos, 1):
+
+# loop over <beta_SD|
+	for beta in range(0, nr_sd, 1):
+
+		beta_list = s_d[beta,1:]
+		# loop over |alpha_SD>
+		for alpha in range(0, nr_sd, 1):
+			
+			# sum_p,q,r,s <pq|v_2body|rs> a_p^+ a_q^+ a_s a_r:
+
 				# Loop over two body me
-			for tbme in range(0, nme, 1):
+			for tbme in range(0, nr_2bme, 1):
+				alpha_list = s_d[alpha,1:]
 				# fetching the p, q, r, s indices from the text file
-				p = two_body_me[tbme,0]
-				q = two_body_me[tbme,1]
-				r = two_body_me[tbme,2]
-				s = two_body_me[tbme,3]
-				v = two_body_me[tbme,6]
+				p = int(two_body_me[tbme,0])
+				q = int(two_body_me[tbme,1])
+				r = int(two_body_me[tbme,2])
+				s = int(two_body_me[tbme,3])
+				v_pqrs = two_body_me[tbme,6]
 				# stating the restriction on v
+				#print v_pqrs
 
-				if v == 0:
-					continue
-				else:
-				# applying the two body operator
-					r = int(round(r))
-					s = int(round(s))
-					q = int(round(q))
-					p = int(round(p))
+				if v_pqrs != 0:
+					#print v_pqrs
+					index_r = -1
+					index_r_bol = True
+					i = 0 # variable to count the position in alpha_list 
+				
+					while index_r_bol and i < N_particles:
+				
+						if r == int(alpha_list[i]):
+							index_r = i
+							index_r_bol = False
+						else:
+							i += 1
 
-					if s in s_d[alpha,1:] and r in s_d[alpha,1:]:
+					if index_r < 0:
+						continue
+
+					alpha_list = np.delete(alpha_list,index_r) # remove r state from list
+
+					index_s = -1
+					index_s_bol = True
+					j = 0 # variable to count the position in alpha_list 
+				
+					while index_s_bol and j < N_particles-1:
+				
+						if s == int(alpha_list[j]):
+							index_s = j
+							index_s_bol = False
+						else:
+							j += 1
+
+					if index_s < 0:
+						continue
+
+					alpha_list = np.delete(alpha_list,index_s) # remove s state from list
+
+					# insert phase that should be related with index_r and index_s and the permutation of a^+ 
+					alpha_list = np.append(alpha_list,p)
+					alpha_list = np.append(alpha_list,q)
+
+					alpha_list = np.sort(alpha_list)
+
+					# if <beta|alpha'> = 1 the matrix element H_diag(p,q) is added to the Hamiltonian matrix
+					if np.array_equal(beta_list,alpha_list):
+						hamiltonian_2body[beta, alpha] = hamiltonian_2body[beta, alpha] + v_pqrs
+			hamiltonian_2body[alpha, beta] = hamiltonian_2body[beta, alpha] 
+	print hamiltonian_2body
+
+'''
+					if s in alpha_list and r in s_d[alpha,1:]:
 						# Temporarily saving variables s, r
 						stemp = s
 						rtemp = r
@@ -176,7 +226,7 @@ def Hamiltonian():
 								neo[beta,alpha] = neo[alpha,beta] + v
 
 						# UP UNTIL HERE IT WORKS
-						"""for slater in range(0,nos,1):
+						"""for slater in range(0,nr_sd,1):
 							beta = s_d[slater, 1:5]
 							#HERE WE NEED A WAY TO PICK ONLY THE SDS THAT HAVE
 							#ALL ELEMENTS DIFFERENT!!! (P!=Q!=R!=S)
@@ -208,6 +258,6 @@ def Hamiltonian():
 					# #phase needs to be entered later
 
 	print neo
-
-
+'''
+Hamiltonian()
 
