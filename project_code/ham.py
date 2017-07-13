c5b750518c997a23d5665eed141ffd12b9bfbec7
Gianluca Salvioni <gianlucasalvioni@gmail.com>
2017-07-13 14:18:23 +0200

    ham: one_body_ham improved

--- project_code/ham.py
+++ project_code/ham.py
@@ -19,62 +19,70 @@ def Hamiltonian_one_body():
 
 	# Read in one-body matrix elements
 	one_body_me = np.genfromtxt(folder_name+"pairing_g1.int", comments = "!", skip_header=2, max_rows=1)[1:5]	
-	
-	# nme is the number of two body matrix elements	
-	nr_matrix_el = one_body_me.shape[0]
+	#print one_body_me
 
 	nr_sp_states = 8
+	Nr_particles = 4
 
+	H_diag = np.zeros((nr_sp_states, nr_sp_states))
+	for i in range(nr_sp_states):
+		H_diag[i,i] = one_body_me[i/2] #this is an integer division 
+	#print H_diag	
 	# neo is the hamiltonian with zero elements
 	hamiltonian = np.zeros((nr_sd, nr_sd))
-
+	#print hamiltonian
+	#sys.exit()
 	# Starting loop over NSD
+	count = 0
 	for beta in range(0, nr_sd, 1):
+		beta_list = s_d[beta,1:]
 		for alpha in range(0, nr_sd, 1):
 				# Loop over two body me
-			for p in range(nr_sp_states):
-				for q in range(nr_sp_states):
+			
+			for p in range(1,nr_sp_states+1):
+				for q in range(1,nr_sp_states+1):
+					alpha_list = s_d[alpha,1:]
+					index = -1
+					index_bol = True
+					i = 0
+				
+					while index_bol and i < Nr_particles:
+				
+						if q == int(alpha_list[i]):
+							index = i
+							index_bol = False
+						else:
+							i += 1
+				
+					count +=1
+				
+					#if index < 0:
+					#	continue		
+					#index = np.argwhere(alpha_list-q)
+					#print index
+					#print alpha_list
+					#sys.exit()
+					alpha_list = np.insert(alpha_list,index,p)
+					#alpha_list = alpha_list.add(p)
+					alpha_list = np.delete(alpha_list,index+1)
+					alpha_list = np.sort(alpha_list)
 
-					#index = np.argwhere(s_d[alpha,1:]-q)
-					print s_d[alpha,1:]
-					print q
-					print index
+					if np.array_equal(beta_list,alpha_list):
+						hamiltonian[beta, alpha] = hamiltonian[beta, alpha] + H_diag[p-1,q-1]
 
-					#s_d[alpha,index] = p
-
-					s_d[alpha,1:5] = np.where(q==s_d[alpha,1:], p, s_d[alpha,1:])
-
-					sys.exit()
+	print hamiltonian
+					
+#					if count > 1:
+#						sys.exit()
 					#if q in s_d[alpha]: #if any s_q[] is equal to q
-
+					
 						
 
 			
-			
+Hamiltonian_one_body()			
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	print hamiltonian
+####################################################################################
 
 
 def Hamiltonian():
@@ -194,4 +202,4 @@ def Hamiltonian():
 	print neo
 
 
-Hamiltonian_one_body()
+
