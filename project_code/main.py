639bee18d67c059544d549a0f1a8b08782c36529
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2017-07-17 15:43:23 +0200

    matrix

--- project_code/main.py
+++ project_code/main.py
@@ -15,18 +15,31 @@ import os.path
 # Choose if you want to interact with the terminal:
 # (uncomment your choice and comment the other):
 
-nmin, nmax, lmin, lmax, jmin, jmax, isos, g, N_particles, sp_basis_filename, SD_filename, tbme_filename = command_line_input()
-#nmin, nmax, lmin, lmax, jmin, jmax, isos, g, N_particles = manual_input()
-#sp_basis_filename, SD_filename, tbme_filename = give_file_names(g)
+#nmin, nmax, lmin, lmax, jmin, jmax, isos, g, N_particles, sp_basis_filename, SD_filename, tbme_filename = command_line_input()
+nmin, nmax, lmin, lmax, jmin, jmax, isos, g, N_particles = manual_input()
+sp_basis_filename, SD_filename, tbme_filename = give_file_names(g)
 
 ##############################################################
-# Checking if the files containing the basis and the slater 
-# determinants  already exist, if not create them:
-if os.path.isfile(sp_basis_filename) == False: 
-    sp_pairing(nmin, nmax, lmin, lmax, jmin, jmax, isos, sp_basis_filename)
+# Deciding if using the sd shell or not:
 
-# Read in the basis from file
-particle, A_core, Z_core, nr_sp_states, nr_groups, nr_sp_n, nr_sp_p, sp_matrix = read_basis(sp_basis_filename)
+sd_shell = 'sd'
+
+if sd_shell == 'sd':
+    sp_sd_shell_matrix = read_sd_basis('table_files/sd_shell.sp')
+    nr_sp_states = np.shape(sp_sd_shell_matrix)[0]
+
+
+elif sd_shell != 'sd':
+    # Checking if the files containing the basis and the slater 
+    # determinants  already exist, if not create them:
+    if os.path.isfile(sp_basis_filename) == False: 
+        sp_pairing(nmin, nmax, lmin, lmax, jmin, jmax, isos, sp_basis_filename)
+
+    # Read in the basis from file
+    particle, A_core, Z_core, nr_sp_states, nr_groups, nr_sp_n, nr_sp_p, sp_matrix = read_basis(sp_basis_filename)
+
+
+##############################################################
 
 if os.path.isfile(SD_filename) == False:
     create_SD(N_particles, nr_sp_states, sp_matrix, SD_filename)
@@ -47,7 +60,7 @@ if os.path.isfile(tbme_filename) == False:
 # The 1-body and 2-body hamiltonian matrices are added together to form hamiltonian_total
 # hamiltonian_total is initialized to zeros
 hamiltonian_total = np.zeros((nr_SD, nr_SD))
-hamiltonian_1body = Hamiltonian_one_body(N_particles, nr_sp_states, SD_filename, tbme_filename)
+hamiltonian_1body = Hamiltonian_one_body(N_particles, nr_sp_states, sp_sd_shell_matrix, SD_filename, tbme_filename)
 hamiltonian_2body = Hamiltonian_two_body(N_particles, nr_sp_states, SD_filename, tbme_filename)
 
 hamiltonian_total = hamiltonian_1body+hamiltonian_2body
