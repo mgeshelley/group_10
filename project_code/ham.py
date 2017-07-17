d53fb2174d9c7a651e77c75bea2d441736312010
mges501York <mges501@york.ac.uk>
2017-07-17 16:12:59 +0200

    Commented out if blocks (work in progress)

--- project_code/ham.py
+++ project_code/ham.py
@@ -130,13 +130,13 @@ def Hamiltonian_two_body(N_particles, nr_sp_states, SD_filename, tbme_filename):
 			alpha_list = s_d[alpha,1:]
 
 			alpha_beta_compare = list( set(alpha_list).symmetric_difference(set(beta_list)) )
-			if len(alpha_beta_compare) == 0:
-				# Sum over i and j (all 2-body matrix elements)
-				for i in range(0,nr_sp_states):
-					
-			elif len(alpha_beta_compare) == 2:
-
-			elif len(alpha_beta_compare) == 4:
+			# if len(alpha_beta_compare) == 0:
+			# 	# Sum over i and j (all 2-body matrix elements)
+			# 	for i in range(0,nr_sp_states):
+			#
+			# elif len(alpha_beta_compare) == 2:
+			#
+			# elif len(alpha_beta_compare) == 4:
 
 			sum_p,q,r,s <pq|v_2body|rs> a_p^+ a_q^+ a_s a_r:
 
