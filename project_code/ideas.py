9c0caefae6322cecd6886c878b839bcb0b5c8b38
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2017-07-18 10:23:10 +0200

    function to find energy of state working

--- project_code/ideas.py
+++ project_code/ideas.py
@@ -6,35 +6,35 @@ import sys
 def find_energy(state, states_matrix, energies):
     N = np.shape(states_matrix)[0]
     states_listed = np.zeros([N,4])
+
+    e_state = "No state found!" #as initial value, could be set to None
     counter = 0
     for i in range(N):
-        states_listed[i] = states_matrix[i,:]
-        idea = state-states_listed[i] #if the states are equal this should be [0,0,0,0]
+        idea = states_matrix[i,:] - state #if the states are equal this should be [0,0,0,0]
         if np.array_equiv(idea, [0,0,0,0]):
             e_state = energies[i]
             counter +=1
-        if counter >= 2:
+        elif counter >= 2:
             sys.exit("Too many energies found! There must be states that are equal (not allowed).")
-    return e_state, states_listed
+            
+    return e_state
 
 
-states_matrix = np.genfromtxt('table_files/sd_mscheme.int', skip_header=3, usecols=(0,1,2,3), dtype='str')
+# Reading in from file, this can also be done inside the function
+states_matrix = np.genfromtxt('table_files/sd_mscheme.int', skip_header=3, usecols=(0,1,2,3))
 energies = np.genfromtxt('table_files/sd_mscheme.int', usecols=(4), skip_header=3)
-
-#N = np.genfromtxt('table_files/sd_mscheme.int', skip_header=2, usecols=(0), max_rows=1, dtype='int') #=len(states_matrix)
 
 
 ### TESTING
-state = [ 1.,   2.,   1.,   2.]
+state = [ 1., 2., 4., 5.]
+energy_state = find_energy(state, states_matrix, energies)
 
-energy_state, states_listed = find_energy(state, states_matrix, energies)
+print 'Test state: ', state
+print 'Energy found for test state:', energy_state
 
-print 'test state: ', state
-print 'energy found for test state:', energy_state
-
-print '\nlist of states: '
-print states_listed[:5]
-print '\nenergies: '
+print '\nList of states: '
+print states_matrix[:5,:]
+print '\nEnergies: '
 print energies[:5]
 
 
