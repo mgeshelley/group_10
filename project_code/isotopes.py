cc9c915fd88abab076f7d5fff2f05a7a4a2bf7fe
Gianluca Salvioni <gianlucasalvioni@gmail.com>
2017-08-29 18:58:25 +0300

    report: new figures

--- project_code/isotopes.py
+++ project_code/isotopes.py
@@ -4,7 +4,7 @@ import sys
 import matplotlib.pyplot as plt
 
 
-label_o17 = np.genfromtxt('O_isotopes_chain_NushellX/o_17b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o17 = np.genfromtxt('O_isotopes_chain_NushellX/o_17b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o17 = np.genfromtxt('O_isotopes_chain_NushellX/o_17b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -17,7 +17,7 @@ for i in range(len(e_o17_code)):
     x_o17_code.extend([1])
 #print x_o17
 
-label_o18 = np.genfromtxt('O_isotopes_chain_NushellX/o_18b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o18 = np.genfromtxt('O_isotopes_chain_NushellX/o_18b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o18 = np.genfromtxt('O_isotopes_chain_NushellX/o_18b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -29,7 +29,7 @@ x_o18_code =[]
 for i in range(len(e_o18_code)):
     x_o18_code.extend([2])
 
-label_o19 = np.genfromtxt('O_isotopes_chain_NushellX/o_19b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o19 = np.genfromtxt('O_isotopes_chain_NushellX/o_19b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o19 = np.genfromtxt('O_isotopes_chain_NushellX/o_19b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -41,7 +41,7 @@ x_o19_code =[]
 for i in range(len(e_o19_code)):
     x_o19_code.extend([3])
 
-label_o20 = np.genfromtxt('O_isotopes_chain_NushellX/o_20b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o20 = np.genfromtxt('O_isotopes_chain_NushellX/o_20b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o20 = np.genfromtxt('O_isotopes_chain_NushellX/o_20b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -53,7 +53,7 @@ x_o20_code =[]
 for i in range(len(e_o20_code)):
     x_o20_code.extend([4])
 
-label_o21 = np.genfromtxt('O_isotopes_chain_NushellX/o_21b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o21 = np.genfromtxt('O_isotopes_chain_NushellX/o_21b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o21 = np.genfromtxt('O_isotopes_chain_NushellX/o_21b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -65,7 +65,7 @@ x_o21_code =[]
 for i in range(len(e_o21_code)):
     x_o21_code.extend([5])
 
-label_o22 = np.genfromtxt('O_isotopes_chain_NushellX/o_22b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o22 = np.genfromtxt('O_isotopes_chain_NushellX/o_22b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o22 = np.genfromtxt('O_isotopes_chain_NushellX/o_22b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -77,7 +77,7 @@ x_o22_code =[]
 for i in range(len(e_o22_code)):
     x_o22_code.extend([6])
 
-label_o23 = np.genfromtxt('O_isotopes_chain_NushellX/o_23b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o23 = np.genfromtxt('O_isotopes_chain_NushellX/o_23b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o23 = np.genfromtxt('O_isotopes_chain_NushellX/o_23b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -89,7 +89,7 @@ x_o23_code =[]
 for i in range(len(e_o23_code)):
     x_o23_code.extend([7])
 
-label_o24 = np.genfromtxt('O_isotopes_chain_NushellX/o_24b.lpt',skip_header=6, usecols=(4),max_rows=5, dtype='string')
+label_o24 = np.genfromtxt('O_isotopes_chain_NushellX/o_24b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
 #print label_o17
 e_o24 = np.genfromtxt('O_isotopes_chain_NushellX/o_24b.lpt',skip_header=6, usecols=(2),max_rows=5)
 #print e_o17
@@ -101,8 +101,72 @@ x_o24_code =[]
 for i in range(len(e_o24_code)):
     x_o24_code.extend([8])
 
+label_o25 = np.genfromtxt('O_isotopes_chain_NushellX/o_25b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
+#print label_o17
+e_o25 = np.genfromtxt('O_isotopes_chain_NushellX/o_25b.lpt',skip_header=6, usecols=(2),max_rows=5)
+#print e_o17
+x_o25 =[]
+for i in range(len(e_o25)):
+    x_o25.extend([9])
+e_o25_code = [-39.922, -36.586, -35.101, -34.026, -33.832]
+x_o25_code =[]
+for i in range(len(e_o25_code)):
+    x_o25_code.extend([9])
+
+label_o26 = np.genfromtxt('O_isotopes_chain_NushellX/o_26b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
+#print label_o17
+e_o26 = np.genfromtxt('O_isotopes_chain_NushellX/o_26b.lpt',skip_header=6, usecols=(2),max_rows=5)
+#print e_o17
+x_o26 =[]
+for i in range(len(e_o26)):
+    x_o26.extend([10])
+e_o26_code = [-40.869, -38.758, -34.849, -33.753, -32.658]
+x_o26_code =[]
+for i in range(len(e_o26_code)):
+    x_o26_code.extend([10])
+
+label_o27 = np.genfromtxt('O_isotopes_chain_NushellX/o_27b.lpt',skip_header=6, usecols=(4),max_rows=1, dtype='string')
+#print label_o17
+e_o27 = np.genfromtxt('O_isotopes_chain_NushellX/o_27b.lpt',skip_header=6, usecols=(2),max_rows=5)
+#print e_o17
+x_o27 =[]
+for i in range(len(e_o27)):
+    x_o27.extend([11])
+e_o27_code = [-38.713, -34.422, -31.431]
+x_o27_code =[]
+for i in range(len(e_o27_code)):
+    x_o27_code.extend([11])
+
+label_o28 = '0'
+#print label_o17
+e_o28 = [-38.886]
+#print e_o17
+x_o28 =[]
+for i in range(len(e_o28)):
+    x_o28.extend([12])
+e_o28_code = [-38.886]
+x_o28_code =[]
+for i in range(len(e_o28_code)):
+    x_o28_code.extend([12])
+
+
+
 fig, ax = plt.subplots()
 #ax.scatter(x_o17, e_o17)
+
+ax.annotate(label_o17, (x_o17[0],e_o17[0]))
+ax.annotate(label_o18, (x_o18[0],e_o18[0]))
+ax.annotate(label_o19, (x_o19[0],e_o19[0]))
+ax.annotate(label_o20, (x_o20[0],e_o20[0]))
+ax.annotate(label_o21, (x_o21[0],e_o21[0]))
+ax.annotate(label_o22, (x_o22[0],e_o22[0]))
+ax.annotate(label_o23, (x_o23[0],e_o23[0]))
+ax.annotate(label_o24, (x_o24[0],e_o24[0]))
+ax.annotate(label_o25, (x_o25[0],e_o25[0]))
+ax.annotate(label_o26, (x_o26[0],e_o26[0]))
+ax.annotate(label_o27, (x_o27[0],e_o27[0]))
+ax.annotate(label_o28, (x_o28[0],e_o28[0]))
+'''
 for i, txt in enumerate(label_o17):
     ax.annotate(txt, (x_o17[i],e_o17[i]))
 
@@ -127,6 +191,19 @@ for i, txt in enumerate(label_o23):
 for i, txt in enumerate(label_o24):
     ax.annotate(txt, (x_o24[i],e_o24[i]))
 
+for i, txt in enumerate(label_o25):
+    ax.annotate(txt, (x_o25[i],e_o25[i]))
+
+for i, txt in enumerate(label_o24):
+    ax.annotate(txt, (x_o26[i],e_o26[i]))
+
+for i, txt in enumerate(label_o27):
+    ax.annotate(txt, (x_o27[i],e_o27[i]))
+
+for i, txt in enumerate(label_o28):
+    ax.annotate(txt, (x_o28[i],e_o28[i]))
+'''
+
 plt.plot(x_o17, e_o17,'b_', markersize=20)
 plt.plot(x_o17_code, e_o17_code,'rx', markersize=5)
 plt.plot(x_o18, e_o18,'b_', markersize=20)
@@ -143,15 +220,22 @@ plt.plot(x_o23, e_o23,'b_', markersize=20)
 plt.plot(x_o23_code, e_o23_code,'rx', markersize=5)
 plt.plot(x_o24, e_o24,'b_', markersize=20)
 plt.plot(x_o24_code, e_o24_code,'rx', markersize=5)
-
+plt.plot(x_o25, e_o25,'b_', markersize=20)
+plt.plot(x_o25_code, e_o25_code,'rx', markersize=5)
+plt.plot(x_o26, e_o26,'b_', markersize=20)
+plt.plot(x_o26_code, e_o26_code,'rx', markersize=5)
+plt.plot(x_o27, e_o27,'b_', markersize=20)
+plt.plot(x_o27_code, e_o27_code,'rx', markersize=5)
+plt.plot(x_o28, e_o28,'b_', markersize=20)
+plt.plot(x_o28_code, e_o28_code,'rx', markersize=5)
 
 # These statements makes a pretty plot:
 plt.xlabel('valence neutrons')
 plt.ylabel('Binding energy respect to 16O g.s. [MeV] ')
 plt.legend(['output NushellX','output our code'], fontsize=12)
-plt.title('O isotopes chain', fontsize=20)
+plt.title('O isotopes chain', fontsize=15)
 plt.xlim(0, 13)
-plt.ylim(-50, 5)
+plt.ylim(-45, 5)
 # This statement shows the plot, but you can also save the figure directly with savefig('filename'):
 #plt.grid()
 plt.show()
